a51f4a11c45021ea10d9d740f6c0bf7f79ce81cb
Zachary Himes <himesz@spu.edu>
2019-04-07 21:51:37 -0700

    Finish integrate method, and Format
    Final formation of code along with addition of integrate method.

--- polynomial.h
+++ polynomial.h
@@ -7,30 +7,29 @@ using std::istream;
 using std::ostream;
 using std::string;
 
-class Polynomial{
-	size_t _degree;
-	float* _coefficients;
-public:
-	Polynomial(size_t);
-	Polynomial(size_t, const float*);
-	Polynomial(const Polynomial&);
-	~Polynomial();
-	const Polynomial Sum(const Polynomial&)const;
-	const Polynomial Subtract(const Polynomial&)const;
-	const Polynomial Minus()const;
-	const Polynomial Multiply(const Polynomial&)const;
-	const Polynomial Divide(const Polynomial&)const;
-	const Polynomial Derive()const;
-	float Evaluate(float)const;
-	float Integrate(float, float)const;
-	const Polynomial& operator=(const Polynomial&);
-	string ToString()const;
-	bool Equals(const Polynomial&)const;
-	ostream& Write(ostream&)const;
-	istream& Read(istream&);
-
+class Polynomial
+{
+        size_t _degree;
+        float* _coefficients;
+    
+    public:
+        Polynomial(size_t);
+        Polynomial(size_t, const float*);
+        Polynomial(const Polynomial&);
+        ~Polynomial();
+        const Polynomial Sum(const Polynomial&)const;
+        const Polynomial Subtract(const Polynomial&)const;
+        const Polynomial Minus()const;
+        const Polynomial Multiply(const Polynomial&)const;
+        const Polynomial Divide(const Polynomial&)const;
+        const Polynomial Derive()const;
+        float Evaluate(float)const;
+        float Integrate(float, float)const;
+        const Polynomial& operator=(const Polynomial&);
+        string ToString()const;
+        bool Equals(const Polynomial&)const;
+        ostream& Write(ostream&)const;
+        istream& Read(istream&);
 };
-
-
 
 #endif
